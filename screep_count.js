d3ee62f1da76836330d246535c14e5b937769edb
Ryan J Dobrynski <ryjedo@gmail.com>
2015-03-29 01:39:09 -0600

    moves creep list out into variable for short circuit (array length is zero) testing.

--- screep_count.js
+++ screep_count.js
@@ -2,8 +2,9 @@ module.exports = {
 
 	screep_count_with_role: function(role){
 		var count = 0;
-		for(var name in Game.creeps) {
-			var creep = Game.creeps[name];
+		var creep_list = Game.creeps;
+		for(var name in creep_list) {
+			var creep = creep_list[name];
 
 			if (creep.role == role) {
 				count++

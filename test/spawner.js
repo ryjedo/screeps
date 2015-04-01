1084214fb900d841d6b44b31c58c0e7a1bb9cc9d
Ryan Dobrynski <rdobrynski@direwolfdigital.com>
2015-04-01 08:39:57 -0600

    .

--- test/spawner.js
+++ test/spawner.js
@@ -5,7 +5,7 @@ var screep_list = config.active_creeps;
 for (var i = screep_list.length - 1; i >= 0; i--) {
 	//console.log(screep_count.with_role(screep_list[i]));
 
-	var creep_conf = config.creeps.$screep_list[i];
+	var creep_conf = config.creeps[screep_list[i]];
 	if (screep_count.with_role(screep_list[i]) < creep_conf.qty){
 		Game.spawns.Spawn1.createCreep(
 			creep_conf.body,

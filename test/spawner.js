42ef45042c29492de6fe3061a49828aa6c417df9
Ryan J Dobrynski <ryjedo@gmail.com>
2015-03-31 22:19:19 -0600

    .

--- test/spawner.js
+++ test/spawner.js
@@ -4,11 +4,13 @@ var screep_list = config.active_creeps;
 
 for (var i = screep_list.length - 1; i >= 0; i--) {
 	console.log(screep_count.with_role(screep_list[i]));
-	if (screep_count.with_role(screep_list[i]) < config.creeps.screep_list[i].qty){
+
+	creep_conf = config.screeps.screep_list[i];
+	if (screep_count.with_role(screep_list[i]) < creep_conf.qty){
 		Game.spawns.Spawn1.createCreep(
-			config.creeps.screep_list[i].body,
-			config.creeps.screep_list[i].name,
-			config.creeps.screep_list[i].role
+			creep_conf.body,
+			creep_conf.name,
+			creep_conf.role
 			);
 	}
 };

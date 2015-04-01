a6af2a4add28a88389f6cb8242bf9d75c6e040ce
Ryan J Dobrynski <ryjedo@gmail.com>
2015-03-31 22:20:23 -0600

    .

--- test/spawner.js
+++ test/spawner.js
@@ -3,7 +3,7 @@ var screep_count = require('screep_count');
 var screep_list = config.active_creeps;
 
 for (var i = screep_list.length - 1; i >= 0; i--) {
-	console.log(screep_count.with_role(screep_list[i]));
+	//console.log(screep_count.with_role(screep_list[i]));
 
 	creep_conf = config.screeps.screep_list[i];
 	if (screep_count.with_role(screep_list[i]) < creep_conf.qty){

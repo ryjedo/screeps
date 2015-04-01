bc6a1c22b8d9719240b220b4bfa060fb9b71e61c
Ryan J Dobrynski <ryjedo@gmail.com>
2015-03-31 22:00:04 -0600

    cleanup

--- test/spawner.js
+++ test/spawner.js
@@ -3,7 +3,7 @@ var screep_count = require('screep_count');
 var screep_list = config.active_creeps;
 
 for (var i = screep_list.length - 1; i >= 0; i--) {
-	if (screep_count.with_role(screep_list[i]) < config.creeps.harvester.qty){
+	if (screep_count.with_role(screep_list[i]) < config.creeps.screep_list[i].qty){
 		Game.spawns.Spawn1.createCreep(
 			config.creeps.harvester.body,
 			config.creeps.harvester.name,

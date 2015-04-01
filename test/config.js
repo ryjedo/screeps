388074afd20ee7fdddf81690f51ee9f5824274d5
Ryan Dobrynski <rdobrynski@direwolfdigital.com>
2015-04-01 08:46:17 -0600

    .

--- test/config.js
+++ test/config.js
@@ -4,19 +4,19 @@ module.exports = {
 			body: [Game.WORK, Game.CARRY, Game.CARRY, Game.MOVE, Game.MOVE], 
 			name: null, 
 			role: {role: 'harvester'},
-			qty:  1, 
+			qty:  2, 
 		},
 		builder: {
 			body: [Game.MOVE, Game.MOVE, Game.MOVE, Game.WORK, Game.CARRY],
 			name: null,
 			role: {role: 'builder'},
-			qty:  1, 
+			qty:  0, 
 		},
 		guard: {
 			body: [Game.MOVE, Game.TOUGH, Game.TOUGH, Game.ATTACK, Game.ATTACK],
 			name: null,
 			role: {role: 'guard'},
-			qty:  1, 
+			qty:  0, 
 		}
 	},
 	active_creeps: ["harvester", "builder", "guard"]
--- test/spawner.js
+++ test/spawner.js
@@ -4,13 +4,12 @@ var screep_list = config.active_creeps;
 
 for (var i = screep_list.length - 1; i >= 0; i--) {
 	//console.log(screep_count.with_role(screep_list[i]));
-
-	var creep_conf = config.creeps[screep_list[i]];
-	if (screep_count.with_role(screep_list[i]) < creep_conf.qty){
+	var creep_type = config.creeps[screep_list[i]];
+	if (screep_count.with_role(screep_list[i]) < creep_type.qty){
 		Game.spawns.Spawn1.createCreep(
-			creep_conf.body,
-			creep_conf.name,
-			creep_conf.role
+			creep_type.body,
+			creep_type.name,
+			creep_type.role
 			);
 	}
 };

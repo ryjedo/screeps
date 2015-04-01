b674a3e8d8c51d308db430634c352180768ad83b
Ryan J Dobrynski <ryjedo@gmail.com>
2015-03-31 20:42:39 -0600

    moves harvester specs out to config.

--- test/config.js
+++ test/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+	creeps: {
+		harvester: "[Game.WORK, Game.CARRY, Game.MOVE], null, {role: 'harvester'}"
+	},
 	stuff: 'a_thing',
 	harv_count: 3
 };
--- test/spawner.js
+++ test/spawner.js
@@ -2,9 +2,5 @@ var config = require('config');
 var screep_count = require('screep_count');
 
 if (screep_count.with_role("harvester") < config.harv_count){
-	Game.spawns.Spawn1.createCreep(
-	[Game.WORK, Game.CARRY, Game.MOVE],
-	null,
-	{role: 'harvester'}
-);
+	Game.spawns.Spawn1.createCreep(config.creeps.harvester);
 }
